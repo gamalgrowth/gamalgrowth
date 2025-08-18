f3fb6b999f07b1e7bd6754e785cede02deec143f
Gamal <m.gamal07.mg@gmail.com>
2025-08-18 21:29:55 +0400

    feat: Implement CSP with Next.js Middleware and nonce

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W4V3RKR6');`,
         }}
+        nonce={requestHeaders.get('x-nonce') || undefined}
       />
       {/* End Google Tag Manager */}
       <body
