995567b17bff859540a97bdaf75d32f389ed38de
Gamal <m.gamal07.mg@gmail.com>
2025-08-18 21:03:26 +0400

    feat: Add CSP for Google Tag Manager and Analytics

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' https://www.googletagmanager.com;
+              connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://www.googletagmanager.com;
+              img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
+              frame-src 'self' https://www.googletagmanager.com;
+              style-src 'self' 'unsafe-inline';
+            `.replace(/\n/g, '').replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
   /* config options here */
 };
 
